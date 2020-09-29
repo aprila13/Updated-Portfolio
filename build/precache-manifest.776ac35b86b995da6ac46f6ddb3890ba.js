894e4209869b10722cbf064918459f4019c040f2
Aprila13 <aprilacraig1@gmail.com>
2020-09-29 16:20:12 -0400

    Add files via upload

---
+++ build/precache-manifest.776ac35b86b995da6ac46f6ddb3890ba.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f56c392b29b3ed247353347d30a68e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291ffc6c890678189578",
+    "url": "/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "a4fac6ef46869b822ef3",
+    "url": "/static/css/main.519b0452.chunk.css"
+  },
+  {
+    "revision": "291ffc6c890678189578",
+    "url": "/static/js/2.ef703670.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ef703670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fac6ef46869b822ef3",
+    "url": "/static/js/main.a5ef383e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "91a6ce49c02969c08141ce7b9518f477",
+    "url": "/static/media/April-Craig-Resume.91a6ce49.pdf"
+  },
+  {
+    "revision": "c12dc52642c62cb6944288217f5620af",
+    "url": "/static/media/Dictionary.c12dc526.png"
+  },
+  {
+    "revision": "562819cc4fc8f7be7968ac54e15c761e",
+    "url": "/static/media/GoodLooks.562819cc.png"
+  },
+  {
+    "revision": "d6556781b119cb86fb0c3114a0796739",
+    "url": "/static/media/LoanCalc.d6556781.png"
+  },
+  {
+    "revision": "f9ae397c15e6719e71bd6158d0213b54",
+    "url": "/static/media/NYTAPI.f9ae397c.png"
+  },
+  {
+    "revision": "754b1015e565ba8c6a344f19768c710a",
+    "url": "/static/media/PortfolioPage.754b1015.png"
+  },
+  {
+    "revision": "7349d3434c11ce6a6b2732d37a875878",
+    "url": "/static/media/Rock.7349d343.png"
+  },
+  {
+    "revision": "f6847f803f1759e081594dd45e1997ff",
+    "url": "/static/media/SoundMachine.f6847f80.png"
+  },
+  {
+    "revision": "325edece0bbd9a717a945bbe5bb2d275",
+    "url": "/static/media/TempApi.325edece.png"
+  },
+  {
+    "revision": "a7c762b3379f798c844b542bc5a1dbfc",
+    "url": "/static/media/TempConverter.a7c762b3.png"
+  },
+  {
+    "revision": "4683504d0145defdf48b1aca5f82a803",
+    "url": "/static/media/landing.4683504d.png"
+  },
+  {
+    "revision": "77db1fb07e510e8f095ffb973c65cfae",
+    "url": "/static/media/me.77db1fb0.jpg"
+  }
+]);
